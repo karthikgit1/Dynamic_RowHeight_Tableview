85d7c4c5e55ab8c0a09acb1f5cc0693fb5e34c3b
karthikeyan <karthikeyan.karthik25@gmail.com>
2015-05-22 16:18:33 +0530

    Initial Commit

---
+++ Dynamic_Row_HeightTests/Dynamic_Row_HeightTests.m
@@ -0,0 +1,40 @@
+//
+//  Dynamic_Row_HeightTests.m
+//  Dynamic_Row_HeightTests
+//
+//  Created by Vy Systems - iOS1 on 5/22/15.
+//  Copyright (c) 2015 Vy Systems - iOS1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Dynamic_Row_HeightTests : XCTestCase
+
+@end
+
+@implementation Dynamic_Row_HeightTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
